163b3a67c258c8b4844e7ec042e0acbfc04add41
omiete <user@users-MacBook-Pro.local>
2023-08-10 15:49:30 +0100

    Video Puases/Plays on click

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,56 +28,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-const video = document.getElementById('hero-video');
-const playPauseButton = document.getElementById('play-pause-button');
-const playPause = document.getElementById('play-pause');
-const mute = document.getElementById('mute');
-const volume = document.getElementById('volume');
-const progressBar = document.querySelector('.progress');
+const video = document.querySelector('video');
 
-playPauseButton.addEventListener('click', togglePlayPause);
-playPause.addEventListener('click', togglePlayPause);
-mute.addEventListener('click', toggleMute);
-volume.addEventListener('input', adjustVolume);
-video.addEventListener('timeupdate', updateProgressBar);
-video.addEventListener('ended', () => playPause.innerHTML = '<i class="fas fa-play"></i>');
+video.muted = true; // Start with video muted
 
-function togglePlayPause() {
+video.addEventListener('click', () => {
     if (video.paused) {
-        video.play();
-        playPause.innerHTML = '<i class="fas fa-pause"></i>';
+        video.muted = false; // Unmute the video
+        video.play(); // Start playing
     } else {
-        video.pause();
-        playPause.innerHTML = '<i class="fas fa-play"></i>';
+        video.pause(); // Pause if playing
     }
-}
-
-function toggleMute() {
-    if (video.muted) {
-        video.muted = false;
-        mute.innerHTML = '<i class="fas fa-volume-up"></i>';
-        volume.value = video.volume;
-    } else {
-        video.muted = true;
-        mute.innerHTML = '<i class="fas fa-volume-mute"></i>';
-        volume.value = 0;
-    }
-}
-
-function adjustVolume() {
-    video.volume = volume.value;
-    if (volume.value == 0) {
-        mute.innerHTML = '<i class="fas fa-volume-mute"></i>';
-    } else {
-        mute.innerHTML = '<i class="fas fa-volume-up"></i>';
-    }
-}
-
-function updateProgressBar() {
-    const progress = (video.currentTime / video.duration) * 100;
-    progressBar.style.width = `${progress}%`;
-}
-
-// window.addEventListener('scroll', () => {
-//     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-//   }, false);
+});
